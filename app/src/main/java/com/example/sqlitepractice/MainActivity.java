c9d476f271113955f406c8575247c1fef49f150a
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-09-07 11:25:43 +0900

    ID deletion and initialization

--- app/src/main/java/com/example/sqlitepractice/MainActivity.java
+++ app/src/main/java/com/example/sqlitepractice/MainActivity.java
@@ -16,28 +16,34 @@ public class MainActivity extends AppCompatActivity {
 try {
     SQLiteDatabase myEvents=this.openOrCreateDatabase("Users",MODE_PRIVATE,null);
 
-    myEvents.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR, age INT(3))");
+    myEvents.execSQL("CREATE TABLE IF NOT EXISTS newUsers(name VARCHAR, age INT(3),id INTEGER PRIMARY KEY)");
 
 
-    /*myEvents.execSQL("INSERT INTO users (name,age) VALUES ('Aziz',22)");
-    myEvents.execSQL("INSERT INTO users (name,age) VALUES ('Akbarshox',18)");
-    myEvents.execSQL("INSERT INTO users (name,age) VALUES ('Lolo',35)");
-    myEvents.execSQL("INSERT INTO users (name,age) VALUES ('Lolo',16)");
-     myEvents.execSQL("DELETE FROM users WHERE name='Lolo' AND age=35");
-    */
+   /* myEvents.execSQL("INSERT INTO newUsers (name,age) VALUES ('Aziz',22)");
+    myEvents.execSQL("INSERT INTO newUsers (name,age) VALUES ('Akbarshox',18)");
+    myEvents.execSQL("INSERT INTO newUsers (name,age) VALUES ('Lolo',35)");
+    myEvents.execSQL("INSERT INTO newUsers (name,age) VALUES ('Lolo',16)");*/
+
+      //  myEvents.execSQL("DELETE FROM newUsers WHERE id=5");
 
 
-    Cursor c = myEvents.rawQuery("SELECT * FROM users", null);
+
+
+
+
+    Cursor c = myEvents.rawQuery("SELECT * FROM newUsers", null);
 
         int nameIndex = c.getColumnIndex("name");
         int ageIndex = c.getColumnIndex("age");
+        int idIndex=c.getColumnIndex("id");
 
         c.moveToFirst();
 
         while (!c.isAfterLast()) {
             Log.i("Name", c.getString(nameIndex));
-
             Log.i("Age", Integer.toString(c.getInt(ageIndex)));
+            Log.i("Id",Integer.toString(c.getInt(idIndex)));
+
             c.moveToNext();
 
         }
